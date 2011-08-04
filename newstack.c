ae2b7009ed79679e6225ca21fba01bb0d69eebb8
Max Bernstein <tekknolagi@gmail.com>
2011-08-04 15:07:40 -0500

    cleaning up

--- newstack.c
+++ newstack.c
@@ -10,26 +10,6 @@ TODO:
 
 */
 
-typedef struct {
-	const char cmd[DIM2];
-	int key;
-} item;
-
-item cmdTable[] = {
-	//{"push",   12}, /* pushes number onto stack */
-	//{"pop",    1}, /* pops number from stack */
-	//{"+",      2}, /* adds top two numbers on stack */
-	{"ifeq",   3}, /* checks if top element is bottom, jumps to <arg> */
-	{"jump",   4}, /* jumps to <arg> */
-	//{"print",  5}, /* prints top element */
-	//{"dup",    6}, /* pushes copy of top element */
-	//{"dec",    7}, /* decrements top element */
-	//{"inc",    8}, /* increments top element */
-	//{"++",     9}, /* sums the elements in the stack */
-	{"while", 10} /* to be implemented */
-	//{"*",     11},
-};
-
 void error(char* msg) { printf("error: %s\n", msg); }
 
 void eval(stackT *res, char cmd[]) {
