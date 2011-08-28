c11c8b31fab777e411899234239976e1e2e7e528
Maxwell Bernstein <tekknolagi@gmail.com>
2011-08-28 14:56:46 -0700

    some benchmarks

--- test.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int f(int x) {
-	if (x < 1) return 1;
-	else return x*f(x-1);
-}
-int main(){
-	printf("%d\n",f(10));
-}
--- test.gecho.c
+++
@@ -1,7 +0,0 @@
-#include <gecho/header.h>
-StackPush(&dataStack, 1.000000);
-StackPush(&dataStack, 10.000000);
-range(&dataStack);
-mulmul(&dataStack);
-showtop(&dataStack);
-}
