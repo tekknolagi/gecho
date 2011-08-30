91d66f58e4687e56e6cee5f064d3aa135a868673
Maxwell Bernstein <tekknolagi@gmail.com>
2011-08-29 21:12:18 -0700

    fixed compiler

--- examples/comparisons.gecho.c
+++ examples/comparisons.gecho.c
@@ -4,7 +4,7 @@ StackPush(&dataStack, 2.000000);
 printf("2 | ");StackShow(&dataStack);
 StackPush(&dataStack, 3.000000);
 printf("3 | ");StackShow(&dataStack);
+lt(&dataStack);
 printf("< | ");StackShow(&dataStack);
 showtop(&dataStack);
-printf(". | ");StackShow(&dataStack);
 }
